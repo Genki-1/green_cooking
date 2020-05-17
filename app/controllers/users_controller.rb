d38f788284a48a27043b26bf2017507396f55246
Genki-1 <60809881+Genki-1@users.noreply.github.com>
2020-05-17 14:24:40 +0900

    [Add]フォロー機能、フォロワー一覧画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,14 +24,12 @@ class UsersController < ApplicationController
     #@userがフォローしているユーザー
     @user  = User.find(params[:id])
     @users = @user.following
-    render 'show_follow'
   end
 
   def followers
     #@userをフォローしているユーザー
     @user  = User.find(params[:id])
     @users = @user.followers
-    render 'show_follower'
   end
 
   private
