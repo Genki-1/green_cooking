bc7c8d0fadda7eaafb9bf019ba8f4eccb6e96fd3
Genki-1 <60809881+Genki-1@users.noreply.github.com>
2020-05-22 23:01:04 +0900

    アプリケーション、ホームコントローラーの修整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,17 +9,19 @@ class ApplicationController < ActionController::Base
     @q = Recipe.ransack(params[:q])
 
     @recipes = @q.result(distinct: true)
-    if current_user.is_meat_status
-      @recipes = @recipes.where.not(is_meat_status: true)
-    end
-    if current_user.is_fish_status
-      @recipes = @recipes.where.not(is_fish_status: true)
-    end
-    if current_user.is_egg_status
-      @recipes = @recipes.where.not(is_egg_status: true)
-    end
-    if current_user.is_dairy_products_status
-      @recipes = @recipes.where.not(is_dairy_products_status: true)
+    if user_signed_in?
+      if current_user.is_meat_status
+        @recipes = @recipes.where.not(is_meat_status: true)
+      end
+      if current_user.is_fish_status
+        @recipes = @recipes.where.not(is_fish_status: true)
+      end
+      if current_user.is_egg_status
+        @recipes = @recipes.where.not(is_egg_status: true)
+      end
+      if current_user.is_dairy_products_status
+        @recipes = @recipes.where.not(is_dairy_products_status: true)
+      end
     end
     @page_recipes = @recipes.page(params[:page]).per(5)
   end
