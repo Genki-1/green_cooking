df3756d88a345013e2e064f4851668a91cbf3732
Genki-1 <60809881+Genki-1@users.noreply.github.com>
2020-05-23 23:14:34 +0900

    [Add]ランキング機能追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,24 +1,34 @@
 class HomesController < ApplicationController
   def index
-    @recipes=Recipe.all
+    @all_ranks = Recipe.joins(:likes).group(:recipe_id)
     if user_signed_in?
       if current_user.is_meat_status
         @recipes = @recipes.where.not(is_meat_status: true)
+        @all_ranks = @all_ranks.where.not(is_meat_status: true)
       end
       if current_user.is_fish_status
         @recipes = @recipes.where.not(is_fish_status: true)
+        @all_ranks = @all_ranks.where.not(is_fish_status: true)
       end
       if current_user.is_egg_status
         @recipes = @recipes.where.not(is_egg_status: true)
+        @all_ranks = @all_ranks.where.not(is_egg_status: true)
       end
       if current_user.is_dairy_products_status
         @recipes = @recipes.where.not(is_dairy_products_status: true)
+        @all_ranks = @all_ranks.where.not(is_dairy_products_status: true)
       end
     end
     @page_recipes = @recipes.page(params[:page]).per(5)
+    food_like_count = @all_ranks.count
+    food_liked_ids = Hash[food_like_count.sort_by{ |_, v| -v }].keys
+    @ranking_recipes = []
+    recipes = Recipe.where(id: food_liked_ids)
+    food_liked_ids[0..2].each do |id|
+      @ranking_recipes << recipes.detect { |r| r.id == id }
+    end
   end
 
   def about
   end
 end
-
